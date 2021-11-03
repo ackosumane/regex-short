73ef895385623bfc99765f45a8d1ffbd4feac719
Aleksandar Marjanović <aleksandar.marjanovic@abstract.rs>
2021-11-03 11:03:12 +0100

    Update names on v2

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
 
-let RegExEmail = str => /^([a-zA-Z]([a-zA-Z0-9\-\_][\.]{0,1}){0,61})?[a-zA-Z0-9\-\_]\@(([a-zA-Z0-9][a-zA-Z0-9\-]{0,61})?[a-zA-Z0-9]\.)?[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.[a-zA-Z0-9]([a-zA-Z0-9\.]{0,61})?[a-zA-Z0-9]$/.test(str);
-exports.RegExEmail = RegExEmail
+let testEmail = str => /^([a-zA-Z]([a-zA-Z0-9\-\_][\.]{0,1}){0,61})?[a-zA-Z0-9\-\_]\@(([a-zA-Z0-9][a-zA-Z0-9\-]{0,61})?[a-zA-Z0-9]\.)?[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.[a-zA-Z0-9]([a-zA-Z0-9\.]{0,61})?[a-zA-Z0-9]$/.test(str);
+exports.testEmail = testEmail
 
-let RegExDomain = str => /^(([a-zA-Z0-9][a-zA-Z0-9\-]{0,61})?[a-zA-Z0-9]\.)?[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.[a-zA-Z0-9]([a-zA-Z0-9\.]{0,61})?[a-zA-Z0-9]$/.test(str);
-exports.RegExDomain = RegExDomain
+let testDomain = str => /^(([a-zA-Z0-9][a-zA-Z0-9\-]{0,61})?[a-zA-Z0-9]\.)?[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.[a-zA-Z0-9]([a-zA-Z0-9\.]{0,61})?[a-zA-Z0-9]$/.test(str);
+exports.testDomain = testDomain
 
-let RegExURL = str => /^http(s)?\:\/\/(www)?(([a-zA-Z0-9][a-zA-Z0-9\-]{0,61})?[a-zA-Z0-9]\.)?[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.[a-zA-Z0-9]([a-zA-Z0-9\.]{0,61})?[a-zA-Z0-9](\S+)?$/.test(str);
-exports.RegExURL = RegExURL
+let testURL = str => /^http(s)?\:\/\/(www)?(([a-zA-Z0-9][a-zA-Z0-9\-]{0,61})?[a-zA-Z0-9]\.)?[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]\.[a-zA-Z0-9]([a-zA-Z0-9\.]{0,61})?[a-zA-Z0-9](\S+)?$/.test(str);
+exports.testURL = testURL
 
-let RegExPhone = str => /^(\+)?([0-9][\s\-/]{0,1})+$/.test(str);
-exports.RegExPhone = RegExPhone
+let testPhone = str => /^(\+)?([0-9][\s\-/]{0,1})+$/.test(str);
+exports.testPhone = testPhone
 
-let RegExHashTag = str => /^\#[a-zA-Z0-9\_]+$/.test(str);
-exports.RegExHashTag = RegExHashTag
+let testHashTag = str => /^\#[a-zA-Z0-9\_]+$/.test(str);
+exports.testHashTag = testHashTag
 
-let RegExBCryptHash = str => /^[a-zA-Z0-9\$\.\/]{60}$/.test(str);
-exports.RegExBCryptHash = RegExBCryptHash
+let testBCryptHash = str => /^[a-zA-Z0-9\$\.\/]{60}$/.test(str);
+exports.testBCryptHash = testBCryptHash
 
-let RegExSHA224 = str => /^[a-z0-9]{56}$/.test(str);
-exports.RegExSHA224 = RegExSHA224
+let testSHA224 = str => /^[a-z0-9]{56}$/.test(str);
+exports.testSHA224 = testSHA224
 
-let RegExSHA256 = str => /^[a-z0-9]{64}$/.test(str);
-exports.RegExSHA256 = RegExSHA256
+let testSHA256 = str => /^[a-z0-9]{64}$/.test(str);
+exports.testSHA256 = testSHA256
 
-let RegExSHA384 = str => /^[a-z0-9]{96}$/.test(str);
-exports.RegExSHA384 = RegExSHA384
+let testSHA384 = str => /^[a-z0-9]{96}$/.test(str);
+exports.testSHA384 = testSHA384
 
-let RegExSHA512 = str => /^[a-z0-9]{128}$/.test(str);
-exports.RegExSHA512 = RegExSHA512
+let testSHA512 = str => /^[a-z0-9]{128}$/.test(str);
+exports.testSHA512 = testSHA512
 
-let RegExJWTToken256 = str => /^[a-zA-Z0-9\.\_\-]{187}$/.test(str);
-exports.RegExJWTToken256 = RegExJWTToken256
+let testJWTToken256 = str => /^[a-zA-Z0-9\.\_\-]{187}$/.test(str);
+exports.testJWTToken256 = testJWTToken256
 
-let RegExJWTToken384 = str => /^[a-zA-Z0-9\.\_\-]{208}$/.test(str);
-exports.RegExJWTToken384 = RegExJWTToken384
+let testJWTToken384 = str => /^[a-zA-Z0-9\.\_\-]{208}$/.test(str);
+exports.testJWTToken384 = testJWTToken384
 
-let RegExJWTToken512 = str => /^[a-zA-Z0-9\.\_\-]{230}$/.test(str);
-exports.RegExJWTToken512 = RegExJWTToken512
+let testJWTToken512 = str => /^[a-zA-Z0-9\.\_\-]{230}$/.test(str);
+exports.testJWTToken512 = testJWTToken512
 
-let RegExUUIDHash = str => /^([a-z0-9]{8})\-([a-z0-9]{4})\-([a-z0-9]{4})\-([a-z0-9]{4})\-([a-z0-9]{12})$/.test(str);
-exports.RegExUUIDHash = RegExUUIDHash
+let testUUIDHash = str => /^([a-z0-9]{8})\-([a-z0-9]{4})\-([a-z0-9]{4})\-([a-z0-9]{4})\-([a-z0-9]{12})$/.test(str);
+exports.testUUIDHash = testUUIDHash
 
-let RegExDigits = str => /\d+/.test(str);
-exports.RegExDigits = RegExDigits
+let testDigits = str => /\d+/.test(str);
+exports.testDigits = testDigits
 
-let RegExAlphanumeric = str => /\w+/.test(str);
-exports.RegExAlphanumeric = RegExAlphanumeric
+let testAlphanumeric = str => /\w+/.test(str);
+exports.testAlphanumeric = testAlphanumeric
 
 exports.default = {
-    RegExEmail: RegExEmail,
-    RegExDomain: RegExDomain,
-    RegExURL: RegExURL,
-    RegExPhone: RegExPhone,
-    RegExHashTag: RegExHashTag,
-    RegExBCryptHash: RegExBCryptHash,
-    RegExSHA224: RegExSHA224,
-    RegExSHA256: RegExSHA256,
-    RegExSHA384: RegExSHA384,
-    RegExSHA512: RegExSHA512,
-    RegExJWTToken256: RegExJWTToken256,
-    RegExJWTToken384: RegExJWTToken384,
-    RegExJWTToken512: RegExJWTToken512,
-    RegExUUIDHash: RegExUUIDHash,
-    RegExDigits: RegExDigits,
-    RegExAlphanumeric: RegExAlphanumeric,
+    testEmail: testEmail,
+    testDomain: testDomain,
+    testURL: testURL,
+    testPhone: testPhone,
+    testHashTag: testHashTag,
+    testBCryptHash: testBCryptHash,
+    testSHA224: testSHA224,
+    testSHA256: testSHA256,
+    testSHA384: testSHA384,
+    testSHA512: testSHA512,
+    testJWTToken256: testJWTToken256,
+    testJWTToken384: testJWTToken384,
+    testJWTToken512: testJWTToken512,
+    testUUIDHash: testUUIDHash,
+    testDigits: testDigits,
+    testAlphanumeric: testAlphanumeric,
 }
